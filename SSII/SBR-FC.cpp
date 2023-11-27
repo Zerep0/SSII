57e0f5ae75ca246455ec1e14bb4b8450ec6914f1
Antonio Pérez <118207492+Zerep0@users.noreply.github.com>
2023-11-27 14:04:35 +0100

    Add files via upload

--- SSII/SBR-FC.cpp
+++ SSII/SBR-FC.cpp
@@ -5,9 +5,15 @@
 #include <sstream>
 using namespace std;
 
+typedef enum{
+    Y, O, NO
+}Enlace;
+
 typedef struct{
     string id;
-    string regla;
+    vector<string> antecedentes;
+    string consecuente;
+    Enlace enlace;
     double FC;
 }Regla;
 
@@ -23,17 +29,36 @@ void leerBC(string BC, vector<Regla> &reglas)
 
     int numeroReglas;
     archivoBC >> numeroReglas;
-    
+
     string linea;
     getline(archivoBC,linea);
     while(getline(archivoBC,linea))
     {
         istringstream lineaRegla(linea);
         Regla regla;
+        regla.enlace = Enlace::NO;
 
         string certeza;
         getline(lineaRegla, regla.id, ':');
-        getline(lineaRegla >> ws, regla.regla, ',');
+
+        string h;
+        while(getline(lineaRegla >> ws, h, ' ') && h != "Entonces")
+        {
+            if(h != "Si" && h != "o" && h != "y")
+            {
+                regla.antecedentes.push_back(h);
+            }else
+            {
+                if(h == "o")
+                {
+                    regla.enlace = Enlace::O;
+                }else if(h == "y")
+                {
+                    regla.enlace = Enlace::Y;
+                }
+            }
+        }
+        getline(lineaRegla, regla.consecuente, ',');
         lineaRegla.ignore(4);
         getline(lineaRegla, certeza, '\n');
         regla.FC = stod(certeza);
@@ -69,6 +94,51 @@ void leerBH(string BH, vector<Hecho> &hechos, string &objetivo)
 
 }
 
+vector<pair<string,string>> consecuentes(vector<Regla> &baseConocimientos,string objetivo)
+{
+
+}
+
+
+void equiparar()
+{
+
+}
+
+
+bool contenido(string objetivo, vector<Hecho> &baseHechos)
+{
+    for(Hecho hecho : baseHechos)
+    {
+        if(hecho.id == objetivo)
+            return true;
+    }
+    return false;
+}
+
+bool verificar(string objetivo, vector<Hecho> &baseHechos)
+{
+    if(contenido(objetivo,baseHechos))
+    {
+        return true;
+    }else{
+
+    }
+
+
+
+
+
+
+}
+
+bool encadenamiento_hacia_atras(vector<Regla> &baseConocimientos, vector<Hecho> &baseHechos, string objetivo)
+{
+    if(verificar(objetivo,baseHechos))
+        return true;
+    else return false;
+}
+
 
 
 int main(int argc, char *argv[])
@@ -76,20 +146,31 @@ int main(int argc, char *argv[])
     vector<Regla> baseConocimientos;
     vector<Hecho> baseHechos;
     string objetivo;
-
     leerBC(argv[1],baseConocimientos);
+    leerBH(argv[2],baseHechos,objetivo);
+
     for(Regla r : baseConocimientos)
+    {
+        cout << "Regla: " << r.id << " FC: " << r.FC << " Consecuente: " << r.consecuente << endl;
+        for(string s : r.antecedentes)
+        {
+            cout << s << ", ";
+        }
+        cout << endl << "Enlace: " << r.enlace << endl;
+    } 
+
+    /*for(Regla r : baseConocimientos)
     {
         cout << r.id << ": " << r.regla << ", FC=" << r.FC << endl;
     }
 
     cout << endl << endl;
 
-    leerBH(argv[2],baseHechos,objetivo);
+
     for(Hecho h : baseHechos)
     {
         cout << h.id << ", FC=" << h.FC << endl;
     }
-    cout << "Objetivo\n" << objetivo << endl;
+    cout << "Objetivo\n" << objetivo << endl;*/
 
 }
